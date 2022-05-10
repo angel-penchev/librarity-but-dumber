c7f39dbcc4c49401f8fbb3f7d9716384967015c7
Angel Penchev <angel.penchev.tsb@gmail.com>
2022-05-10 15:58:46 +0300

    [Test] Book default constructor test
    Added a test that verifies the Book default constructor is functioning
    correctly.

--- test/unit/BookTest.cpp
+++ test/unit/BookTest.cpp
@@ -2,3 +2,15 @@
 // Created by apenchev on 5/9/2022.
 //
 #include "gtest/gtest.h"
+#include "book/Book.h"
+
+TEST(BookTestSuite, ShouldExposeADefaultConstructor) {
+    Book book;
+
+    ASSERT_STREQ(book.getName(), "Untitled");
+    ASSERT_STREQ(book.getAuthor(), "Unknown author");
+    ASSERT_STREQ(book.getDescription(), "");
+    ASSERT_EQ(book.getRating(), 0);
+    ASSERT_STREQ(book.getISBN(), "9780000000000");
+    ASSERT_STREQ(book.getFilename(), "");
+}
