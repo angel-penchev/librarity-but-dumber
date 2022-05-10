e0816291b6b68aa124823eccd716b6e7880dab6a
Angel Penchev <angel.penchev.tsb@gmail.com>
2022-05-10 17:36:56 +0300

    [Test] Book parameter constructor test
    Added a test which verifies the book can be constructed with it's
    properties.

--- test/unit/BookTest.cpp
+++ test/unit/BookTest.cpp
@@ -1,8 +1,29 @@
 //
 // Created by apenchev on 5/9/2022.
 //
+#include <cstring>
 #include "gtest/gtest.h"
 #include "book/Book.h"
+
+class BookFixture : public ::testing::Test {
+protected:
+    Book *book;
+
+    const char *name = "gosho";
+    const char *author = "losho";
+    const char *description = "never gonna give you up";
+    const unsigned int rating = 8;
+    const char *ISBN = "9780000000000";
+    const char *filename = "gosho.txt";
+
+    void SetUp() override {
+        book = new Book(name, author, description, rating, ISBN, filename);
+    }
+
+    void TearDown() override {
+        delete book;
+    }
+};
 
 TEST(BookTestSuite, ShouldExposeADefaultConstructor) {
     Book book;
@@ -13,4 +34,13 @@ TEST(BookTestSuite, ShouldExposeADefaultConstructor) {
     ASSERT_EQ(book.getRating(), 0);
     ASSERT_STREQ(book.getISBN(), "9780000000000");
     ASSERT_STREQ(book.getFilename(), "");
+}
+
+TEST_F(BookFixture, ShouldExposeAConstructorAcceptingParameterValues) {
+    ASSERT_STREQ(book->getName(), name);
+    ASSERT_STREQ(book->getAuthor(), author);
+    ASSERT_STREQ(book->getDescription(), description);
+    ASSERT_EQ(book->getRating(), rating);
+    ASSERT_STREQ(book->getISBN(), ISBN);
+    ASSERT_STREQ(book->getFilename(), filename);
 }
