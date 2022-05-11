a5bdc4cc3f4ac987a02c054be0d7de655991b443
Angel Penchev <angel.penchev.tsb@gmail.com>
2022-05-11 03:46:33 +0300

    [Refactor] User login in main
    Refactored the program to request user credentials instead of a master
    password.

--- main.cpp
+++ main.cpp
@@ -3,39 +3,36 @@
 #include "library/Library.h"
 
 #define MAX_PWD_ATTEMPTS 3
-#define MAX_PWD_LEN 128
 #define MAX_CMD_LEN 16
 #define MAX_STR_LEN 1024
 
 int main() {
     const char *BOOKS_FILENAME = "books.bin";
     const char *USERS_FILENAME = "users.bin";
-    const char *ADMIN_PWD = "gosholosho"; // Really making it easy for the ones out there with a decompiler :>
 
-    bool isAdmin = false, isAttemptingAdmin;
-    char passwordInput[MAX_PWD_LEN];
+    Library library = Library(BOOKS_FILENAME, USERS_FILENAME);
 
-    char isAttemptingAdminInput;
-    std::cout << "Do you want to login as admin? (y/N): ";
-    std::cin >> isAttemptingAdminInput;
-    isAttemptingAdmin = isAttemptingAdminInput == 'Y' || isAttemptingAdminInput == 'y';
+    char usernameInput[MAX_STR_LEN];
+    char passwordInput[MAX_STR_LEN];
+    User *user;
 
-    for (unsigned int i = 0; i < MAX_PWD_ATTEMPTS && isAttemptingAdmin; i++) {
-        std::cout << "Enter administrator password: ";
-        std::cin.ignore();
-        std::cin.getline(passwordInput, MAX_PWD_LEN - 1);
-        if (!std::strcmp(passwordInput, ADMIN_PWD)) {
-            isAdmin = true;
+    std::cout << "Username: ";
+    std::cin.getline(usernameInput, MAX_STR_LEN - 1);
+
+    for (unsigned int i = 0; i < MAX_PWD_ATTEMPTS; i++) {
+        std::cout << "Password: ";
+        std::cin.getline(passwordInput, MAX_STR_LEN - 1);
+        user = library.loginUser(usernameInput, passwordInput);
+
+        if (user != nullptr) {
             break;
         }
     }
 
-    if (isAttemptingAdmin && !isAdmin) {
-        std::cerr << "Failed to login as admin!\n";
+    if (user == nullptr) {
+        std::cerr << "ERR: Failed to authenticate user!\n";
         return 1;
     }
-
-    Library library = Library(BOOKS_FILENAME, USERS_FILENAME);
 
     char command[MAX_CMD_LEN];
     while (std::cout << "|> " && std::cin >> command && std::cin.ignore()) {
@@ -72,7 +69,7 @@ int main() {
         }
 
         if (!std::strcmp(command, "add")) {
-            if (!isAdmin) {
+            if (!user->isAdministrator()) {
                 std::cerr << "ERR: Admin privileges required!\n";
                 continue;
             }
@@ -109,7 +106,7 @@ int main() {
         }
 
         if (!std::strcmp(command, "remove")) {
-            if (!isAdmin) {
+            if (!user->isAdministrator()) {
                 std::cerr << "ERR: Admin privileges required!\n";
                 continue;
             }
